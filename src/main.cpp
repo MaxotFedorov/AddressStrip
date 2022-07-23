21208575885a332b53e1979d4d006b1b06ccfa9e
MaxotFedorov <maxot007@gmail.com>
2022-07-23 16:28:44 +0300

    Include fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
-#include "LittleFS.h"
+#include <LittleFS.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include <FastLED.h>
-#include "GyverTimer.h"
-#include "MyJSONparser.h"
-#include "MyEffectsForWS2812B.h"
+#include <GyverTimer.h>
+#include <MyJSONparser.h>
+#include <MyEffectsForWS2812B.h>
 
 #define LED_PIN 2
 #define NUM_LEDS 60
